0e8fb01f88e8202a61b6e8d1327a458d2b2ebb1b
sectoroverload2k <anthony@linsday.net>
2023-10-09 13:18:44 -0400

    Create NotFoundException.php

---
+++ src/OneClickDNS/Exceptions/NotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace OneClickDNS\Exceptions;
+
+/**
+ * Thrown when the OneClickDNS API returns a 404 Not Found.
+ * @package OneClickDNS\Exceptions
+ */
+class NotFoundException extends HttpException
+{
+
+}
